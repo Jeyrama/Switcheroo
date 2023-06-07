10f3e971d4ace55cf291385254745517b313b967
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-07 10:49:36 -0700

    Update Switcheroo.js

--- Switcheroo.js
+++ Switcheroo.js
@@ -14,3 +14,12 @@ Example:
 
 const switcheroo=x=>x.replace(/[ab]/g,x=>x=="a"?"b":"a")
 
+// or
+
+function switcheroo(x) {
+  return x.split('').map(function(e) {
+    if (e == 'b') return 'a';
+    if (e == 'a') return 'b';
+    if (e == 'c') return 'c'
+  }).join('');
+}
